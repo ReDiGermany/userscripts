fd369617e20e5c98163d404ef1b481870fd754b5
Max Kruggel <support@redigermany.de>
2021-07-30 12:45:04 +0200

    Update version Test

--- notenbekanntgabe.user.js
+++ notenbekanntgabe.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Notenbekanntgabe
 // @namespace    https://github.com/ReDiGermany/userscripts
-// @version      2.0
+// @version      2.1
 // @description  Refreshes the "Notenbekanntgabe" and adds a quick summery including the weighted average grade in the title.
 // @author       Max 'ReDiGermany' Kruggel
 // @match        https://www3.primuss.de/cgi-bin/pg_Notenbekanntgabe/index.pl
