78deea9c55398a81611cf7a7c6cf26243be3ce80
Max Kruggel <support@redigermany.de>
2021-08-02 10:58:36 +0200

    Tried to beautify the code for better readability

--- notenbekanntgabe.user.js
+++ notenbekanntgabe.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Notenbekanntgabe
 // @namespace    https://github.com/ReDiGermany/userscripts
-// @version      3.3
+// @version      3.4
 // @description  Refreshes the "Notenbekanntgabe" and adds a quick summery including the weighted average grade in the title.
 // @author       Max 'ReDiGermany' Kruggel
 // @match        https://www3.primuss.de/cgi-bin/pg_Notenbekanntgabe/index.pl
@@ -10,7 +10,15 @@
 // @updateURL    https://github.com/ReDiGermany/userscripts/raw/main/notenbekanntgabe.user.js
 // ==/UserScript==
 
+HTMLElement.prototype.numberIfy = function(){
+    try{
+        return parseFloat(this.innerText.replace(/,/,"."))
+    }catch(e){}
+    return 0;
+}
+
 class PrimussNotenbekanntgabe {
+    ects={}
 
     // Getting input value by its name
     getInputValueByName(q){
@@ -40,6 +48,25 @@ class PrimussNotenbekanntgabe {
         http.send(params);
     }
 
+    // Parses the HTML table to a object of ects {name:ects}
+    parseECTS(html){
+        let ret = {};
+        const table = html.querySelectorAll("table")[5];
+        const rows = table.querySelectorAll("tr");
+
+        for(let i=1;i<rows.length;i++){
+            const td = rows[i].querySelectorAll("td,th");
+            try{
+                const grade = td[td.length-2].numberIfy();
+                const ects = td[td.length-1].numberIfy();
+                if(isNaN(grade) && !isNaN(ects)){
+                    ret[td[0].innerText] = ects;
+                }
+            }catch(e){}
+        }
+        return ret;
+    }
+
     // Gets the ETCS for weighted grades
     getECTS(callback){
         this.postData('/cgi-bin/Notenblatt/index.pl',{
@@ -48,131 +75,169 @@ class PrimussNotenbekanntgabe {
             Language: "de",
             FH: this.getInputValueByName("FH"),
             Portal: 1
-        },function(html){
-
-            var ret = {};
-            var table = html.querySelectorAll("table")[5];
-            var rows = table.querySelectorAll("tr");
-
-            for(var i=1;i<rows.length;i++){
-                var td = rows[i].querySelectorAll("td,th");
-                try{
-                    var grade = parseFloat(td[td.length-2].innerText.replace(/,/,"."));
-                    var ects = parseFloat(td[td.length-1].innerText.replace(/,/,"."));
-                    if(isNaN(grade)){
-                        if(!isNaN(ects)) ret[td[0].innerText] = ects;
-                    }
-                }catch(e){}
-            }
-
-            callback(ret);
+        }, html => {
+            console.log("before",this.ects)
+            this.ects = this.parseECTS(html);
+            console.log("after",this.ects)
+            callback();
         })
     }
 
+    getSubjectECTS(subjectName,gradeable){
+        let manual = false;
+        let ectsItem = 1;
+        const localECTS = localStorage.getItem(subjectName);
+        if(this.ects.hasOwnProperty(subjectName)){
+            ectsItem = this.ects[subjectName]
+        }else{
+            if(localECTS!=null){
+                ectsItem = parseFloat(localECTS.replace(/,/,'.'))
+            }else{
+                if(gradeable){
+                    this.notFound.push(subjectName);
+                    this.weightedPossible = false;
+                }
+            }
+            manual = true;
+        }
+        return [ectsItem,manual,localECTS]
+    }
+
+    weightedPossible = true;
+    notFound = [];
+    html = {
+        weighted_grade: (grade,ectsItem)=>{
+            return ` <small class='redi_weighted_grade' style='opacity: .5'>(gewichtet: ${grade*ectsItem} | ects: ${ectsItem})</small>`
+        },
+        manual_box: (subjectName,localECTS)=>{
+            return `<input
+            onchange='(function(t){ localStorage.setItem(\"${subjectName}\",t.value); })(this);return false;'
+            name='${subjectName}'
+            class='redi_input'
+            placeholder='ects'
+            style='width: 30px'
+            type='text'
+            value='${localECTS==null?"":localECTS}' />`
+        },
+        table_sum: (d)=>{
+            return `<tr>
+                <td colspan="5">
+                    Last Check: ${d}
+                </td>
+                <td>
+                    Ungewichtet: ${this.unweigted_total_grades/this.number_found_grades}
+                    <b class="redi_weighted_grade"><br />Gewichtet: ${this.weighted_grade/this.ects_total}</b>
+                </td>
+                <td></td>
+            </tr>`
+        }
+    };
+
+    number_found_grades = 0;
+    weighted_grade = 0;
+    ects_total = 0;
+    unweigted_total_grades = 0;
+    number_total_grades=0;
+
+    gradeIfy(gradeable,grade,manual,localECTS,ectsItem){
+        if(gradeable){
+            try{
+                const grd = grade.numberIfy();
+                this.unweigted_total_grades += grd;
+                this.ects_total += ectsItem;
+                this.weighted_grade += grd*ectsItem;
+                grade.innerHTML += this.html.weighted_grade(grd,ectsItem);
+            }catch(e){}
+            this.number_found_grades++;
+            if(manual){
+                td[td.length-2].innerHTML += this.html.manual_box(subjectName,localECTS);
+            }
+        } else grade.setAttribute("style","opacity: 0");
+    }
+
+    checkAdmission(grade,td){
+        if(grade.innerHTML=="erfolgreich"){
+            this.number_total_grades--;
+            for(let j=0;j<td.length;j++){
+                td[j].setAttribute("style","background: rgba(0,0,0,.1)");
+            }
+        }
+    }
+
+    addHooks(){
+        const inputs = document.querySelectorAll(".redi_input");
+        for(let inpi = 0;inpi<inputs.length;inpi++){
+            inputs[inpi].addEventListener("change",function(){ console.log("change") },false);
+        }
+    }
+
     // Parses the current grade board and calculates the weighted average grade
-    parseData(ects){
+    parseData(){
         //console.log(ects)
 
-        var number_found_grades = 0;
-        var number_total_grades=0;
-        var table_rows = document.querySelectorAll(".table2 tbody:last-of-type tr");
-        var weighted_grade = 0;
-        var ects_total = 0;
-        var unweigted_total_grades = 0;
-        var weightedPossible = true;
-        var notFound = [];
+        // (Re)Set data
+        this.number_found_grades = 0;
+        this.weighted_grade = 0;
+        this.ects_total = 0;
+        this.unweigted_total_grades = 0;
+        const table_rows = document.querySelectorAll(".table2 tbody:last-of-type tr");
+        this.number_total_grades=table_rows.length;
+        this.weightedPossible = true;
+        this.notFound = [];
 
-        for(var i=0;i<table_rows.length;i++){
-            var ectsItem = 1;
-            var td = table_rows[i].querySelectorAll("td");
-            var subjectName = td[2].innerHTML;
-            var manual = false;
-            var grade = td[td.length-2].getElementsByTagName("b")[0];
-            var gradeable = grade.innerHTML!="Korrektur noch nicht abgeschlossen" && grade.innerHTML!="erfolgreich";
+        for(let i=0;i<table_rows.length;i++){
+            const td = table_rows[i].querySelectorAll("td");
+            const subjectName = td[2].innerHTML;
+            const grade = td[td.length-2].getElementsByTagName("b")[0];
+            const gradeable = grade.innerHTML!="Korrektur noch nicht abgeschlossen" && grade.innerHTML!="erfolgreich";
 
-            //console.log(subjectName,ects.hasOwnProperty(subjectName),ects[subjectName])
-            if(ects.hasOwnProperty(subjectName)){
-                ectsItem = ects[subjectName]
-            }else{
-                var localECTS = localStorage.getItem(subjectName);
-                if(localECTS!=null){
-                    ectsItem = parseFloat(localECTS.replace(/,/,'.'))
-                }else{
-                    if(gradeable){
-                        notFound.push(subjectName);
-                        weightedPossible = false;
-                    }
-                }
-                manual = true;
-            }
-
-            number_total_grades++;
+            const [ectsItem,manual,localECTS] = this.getSubjectECTS(subjectName,gradeable);
 
             // Outline admission requirements
-            if(grade.innerHTML=="erfolgreich"){
-                number_total_grades--;
-                for(var j=0;j<td.length;j++) td[j].setAttribute("style","background: rgba(0,0,0,.1)");
-            }
-
-            var inputs = document.querySelectorAll(".redi_input");
-            for(var inpi = 0;inpi<inputs.length;inpi++){
-                inputs[inpi].addEventListener("change",function(){ console.log("change") },false);
-            }
+            this.checkAdmission(grade,td);
 
             // Adding graded info to HTML and calculating average grades
-            if(gradeable){
-                try{
-                    var grd = parseFloat(grade.innerText.replace(/,/,'.'));
-                    unweigted_total_grades += grd;
-                    ects_total += ectsItem;
-                    weighted_grade += grd*ectsItem;
-                    grade.innerHTML += " <small class='redi_weighted_grade' style='opacity: .5'>(gewichtet: "+grd*ectsItem+" | ects: "+ectsItem+")</small>";
-                }catch(e){}
-                number_found_grades++;
-                if(manual){
-                    td[td.length-2].innerHTML += "<input onchange='(function(t){ console.log(\"test\",t.value); localStorage.setItem(\""+subjectName+"\",t.value); })(this);return false;' name='"+subjectName+"' class='redi_input' placeholder='ects' style='width: 30px' type='text' value='"+(localECTS==null?"":localECTS)+"' />";
-                }
-            } else grade.setAttribute("style","opacity: 0");
+            this.gradeIfy(gradeable,grade,manual,localECTS,ectsItem)
+
+            this.addHooks();
         }
 
-        var d = new Date().toLocaleTimeString();
+        this.hideIfUnweighted();
+        this.showSummery();
+    }
 
-        document.querySelectorAll(".table2 tbody:last-of-type")[0].innerHTML +=
-            '<tr><td colspan="5">Last Check: '+d+'</td><td>Ungewichtet: '
-            +(unweigted_total_grades/number_found_grades)
-            +'<b class="redi_weighted_grade"><br />Gewichtet: '
-            +(weighted_grade/ects_total)
-            +'</b></td><td></td></tr>';
+    showSummery(){
+        const date = new Date().toLocaleTimeString();
+        document.querySelectorAll(".table2 tbody:last-of-type")[0].innerHTML += this.html.table_sum(date);
 
-        if(!weightedPossible){
-            var d1 = document.getElementsByClassName("redi_weighted_grade");
-            for(var i1=0;i1<d1.length;i1++){
+        // Adding meta info
+        let grade = (this.weighted_grade/this.ects_total);
+        if(!this.weightedPossible) grade = (this-unweigted_total_grades/this.number_found_grades);
+        document.title = `${this.number_found_grades}/${this.number_total_grades} (${date}) (${grade}) :: Primuss`;
+    }
+
+    hideIfUnweighted(){
+        if(!this.weightedPossible){
+            const d1 = document.getElementsByClassName("redi_weighted_grade");
+            for(let i1=0;i1<d1.length;i1++){
                 d1[i1].style.display="none";
             }
         }
 
-        // Adding meta info
-        var g = (weighted_grade/ects_total);
-        if(!weightedPossible) g = (unweigted_total_grades/number_found_grades);
-        document.title = number_found_grades+'/'+number_total_grades+' ('+d+') ('+g+') :: Primuss';
     }
 
-    show(ects){
+    show(){
         var L = document.getElementById("Notenspiegel");
         if(L==null || (L!=null && (L.innerHTML=='' || L.style.display=="none"))){
-            const t = this;
             shownoten();
-            setTimeout(function(){
-                t.parseData(ects);
-            },1000)
+            setTimeout(()=>{ this.parseData() },1000)
         }
     }
 
     constructor() {
-        const t = this;
-        this.getECTS(function(ects){
-            t.show(ects)
-            setInterval(function(){ t.show(ects) },15*1000);
+        this.getECTS(()=>{
+            this.show()
+            setInterval(()=>{ this.show() },15*1000);
         })
     }
 }
